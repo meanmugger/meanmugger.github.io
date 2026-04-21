9c1b2f6fa0d0f529d4ea2aa9d8966b1dc2e3b8bd
meanmugger <itsadud50@gmail.com>
2026-04-20 23:16:23 -0500

    Update Homework3.js

--- MIS3371/Homework3/Homework3.js
+++ MIS3371/Homework3/Homework3.js
@@ -188,7 +188,7 @@ function checkUserId() {
                 document.getElementById("user_message").innerHTML = "Invalid # of characters";
                 error_flag = 1;
             break;
-        case (!/^\b\w/).test(x):           
+        case (!/^\b\w/.test(x)):           
                 document.getElementById("user_message").innerHTML = "First character must not be a number";
                 error_flag = 1;
             break;
